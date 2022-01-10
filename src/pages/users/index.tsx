d4d54c62e2b26d8f2cda27ddb183eef05abdcc65
Ricardo M <ricardo.machado10.rm@gmail.com>
2022-01-10 10:41:32 -0300

    finalizado a paginação UsersList

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Heading, Button, Icon, Table, Thead, Tbody, Tr, Th, Td, Checkbox, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
+import { useState } from "react";
 
 import { RiAddLine, RiPencilLine, RiDeleteBinLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
@@ -9,7 +10,8 @@ import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
 
-    const { data, isLoading, isFetching ,error } = useUsers();
+    const [page, setPage] = useState(1);
+    const { data, isLoading, isFetching ,error } = useUsers(page);
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -66,7 +68,7 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    {data.map(user => {
+                                    {data.users.map(user => {
                                         return (
                                             <Tr key={user.id}>
                                                 <Td px={["4", "4", "6"]}>
@@ -106,7 +108,11 @@ export default function UserList() {
                                 </Tbody>
                             </Table>
 
-                            <Pagination />
+                            <Pagination 
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
+                            />
                         </>
                     )}
                 </Box>
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,5 +1,5 @@
 import faker from 'faker';
-import { ActiveModelSerializer, createServer, Factory, Model } from 'miragejs';
+import { ActiveModelSerializer, createServer, Factory, Model, Response } from 'miragejs';
 
 type User = {
     name: string;
@@ -9,9 +9,9 @@ type User = {
 
 export function makeServer () {
     const server = createServer({
-        serializers: {
+        /*serializers: {
             application: ActiveModelSerializer,
-        },
+        },*/
 
         models: {
             user: Model.extend<Partial<User>>({})
@@ -32,14 +32,30 @@ export function makeServer () {
         },
 
         seeds(server) {
-            server.createList('user', 10)
+            server.createList('user', 100)
         },
         
         routes(){
             this.namespace = 'api';
             this.timing = 750;
 
-            this.get('/users');
+            this.get('/users', function (schema, request) {
+                const { page = 1, per_page = 10 } = request.queryParams
+
+                const total = schema.all('user').length
+
+                const pageStart = (Number(page) -1) * Number(per_page);
+                const pageEnd = pageStart + Number(per_page);
+
+                const users = this.serialize(schema.all('user')).users.slice(pageStart, pageEnd)
+
+                return new Response(
+                    200,
+                    { 'x-total-count': String(total) },
+                    { users }
+                )
+            });
+
             this.post('/users');
 
             this.namespace = '';
